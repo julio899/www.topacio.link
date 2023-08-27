29e3c7dc53199a1f77c556f07c3f61928fac12de
Julio Vinachi <julio899@gmail.com>
2023-08-27 22:26:24 +0000

    fix espaciado menu lateral

---
+++ assets/SubscriptionView-d26e4c01.js
@@ -0,0 +1,1 @@
+import{_ as s,a as o,f as t,p as c,b as _,d as a}from"./index-9b7200bd.js";const n={},i=e=>(c("data-v-687106f6"),e=e(),_(),e),p={class:"subscription"},d=i(()=>a("h1",null,"Subscription IT'S COMING",-1)),r=[d];function u(e,f){return o(),t("main",p,r)}const h=s(n,[["render",u],["__scopeId","data-v-687106f6"]]);export{h as default};
